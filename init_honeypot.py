f210f12fd6182f71a3706ea66cb484669b0fbfb5
Adam McHugh <adam@mchughsecurity.com>
2022-04-14 23:11:16 +0930

    2022-04-14 Development Commit

--- init_honeypot.py
+++ init_honeypot.py
@@ -1,10 +1,15 @@
+import sys
 import ConfigParser
 
 class HoneyPotClass:
   def __init__(self, listenAddress='0.0.0.0', listenPort=445, configFile=None):
-    if configFile == None:
-      print "[*] Config File Required"
+    if configFile is None:
+      print ("[*] Config File Required")
       sys.exit(0)
-    self.__smbConfig = configFile
-    self.__server = SMBSERVER((listenAddress, listenPort), config_parser=self.__smbConfig)
-    self.__server.processConfigFile()
+    print ("[*] Honeypot test fire")
+
+def main():
+  honeypot = HoneyPotClass()
+
+if __name__ is "__main__":
+  main()
